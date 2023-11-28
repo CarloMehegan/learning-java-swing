d44b2cd04f7d398e5f832ce04064c19a8683a229
Carlo <CarloMehegan@gmail.com>
2023-11-28 16:22:50 -0500

    creating jlabels

--- Learning Java Swing.md
+++ Learning Java Swing.md
@@ -4,6 +4,30 @@ description::
 
 ## Learning Java Swing
 
+JFrame
+- a gui window to add components to
+- https://youtu.be/7GaAW-DdPuI first video in series
+
+```Java
+JFrame frame = new JFrame();
+frame.setTitle("JFrame title goes here");
+
+//EXIT instead of HIDE on closing the window
+frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+frame.setResizable(false);
+frame.setSize(400,400); //sets the x dimension and y dimension
+frame.setVisible(true);
+
+ImageIcon image = new ImageIcon("assets/rubiks.png");
+frame.setIconImage(image.getImage()); //change icon of frame
+
+frame.getContentPane().setBackground(new Color(158, 231, 245)); //change color of background
+```
+
+JLabel
+- a gui display area for a a string of text, an image, or both
+- second video
 
 
 
--- src/learningjavaswing/Main.java
+++ src/learningjavaswing/Main.java
@@ -1,30 +1,45 @@
 package learningjavaswing;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.border.Border;
+
 import java.awt.Color;
+import java.awt.Font;
+
+import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// https://youtu.be/7GaAW-DdPuI
+		//JLabel = a GUI display area for a a string of text, an image, or both
 		
-		/*
+		ImageIcon image = new ImageIcon("assets/redpikmin.png");
+		Border border = BorderFactory.createLineBorder(Color.red, 3);
+		
+		JLabel label = new JLabel();
+		label.setText("hello world!!!!!!!!!!!!!!!"); //set text. can also put string in constructor on line above
+		label.setIcon(image);
+		label.setHorizontalTextPosition(JLabel.CENTER); //set text LEFT CENTER or RIGHT of image
+		label.setVerticalTextPosition(JLabel.TOP);
+		label.setForeground(Color.pink);
+		label.setFont(new Font("MV Boli", Font.PLAIN, 20)); //set font
+		label.setIconTextGap(-300); //set gap between image and text
+		label.setBackground(Color.black);
+		label.setOpaque(true);
+		label.setBorder(border);
+		label.setVerticalAlignment(JLabel.CENTER); //sets the position of the icon and text within the label
+		label.setHorizontalAlignment(JLabel.CENTER);
+		//label.setBounds(0, 0, 250, 250); //set position within frame and dimensions
+		
+		
 		JFrame frame = new JFrame();
-		frame.setTitle("JFrame title goes here");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //EXIT instead of HIDE on closing the window
-		frame.setResizable(false);
-		frame.setSize(400,400); //sets the x dimension and y dimension
+		//frame.setSize(400,800); //sets the x dimension and y dimension
+		//frame.setLayout(null);
 		frame.setVisible(true);
-		
-		ImageIcon image = new ImageIcon("assets/rubiks.png");
-		frame.setIconImage(image.getImage()); //change icon of frame
-		
-		frame.getContentPane().setBackground(new Color(158, 231, 245)); //change color of background
-		*/
-		
-		//we can take the above code and put it in a class that extends JFrame
-		//then we run the constructor and its the same as doing the above
-		new MyFrame();
+		frame.add(label);
+		frame.pack(); //size of the frame will adjust to fit all components. AKA don't need to do setSize
 	}
 
 }
